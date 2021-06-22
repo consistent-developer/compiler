1dc8c86983f118beda09610373801547d1b3eb82
Bhayu <connect.github.me@gmail.com>
2021-06-22 19:50:36 +0530

    'project_added'

---
+++ app/temp/1fea5a9.c
@@ -0,0 +1,5 @@
+int main()
+{
+    printf("Hello, World!\n");//Use to get one character input from user, and it will not be printed on screen.
+    return 0;
+}
---
+++ app/temp/5b1dda4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Helsslo World!";
+    return 0;
+}
+}
